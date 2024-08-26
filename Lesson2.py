5a51bbdbc05e4b3ece6ceacc68e5bf8239e044eb
nalog-debug <miha_s_fc_98@mail.ru>
2024-08-26 20:31:37 +0300

    les2 up

---
+++ Lesson2.py
@@ -0,0 +1,7 @@
+echo "# Lesson2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nalog-debug/Lesson2.git
+git push -u origin main
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-a = "Lesson"
-print(a[0])
-print(a[-1])
-print(a[3:])
-print(a[::-1])
-print(a[1] + a[3] + a[5])
+Number_of_completed_HA = 12
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_for_one_task = (Number_of_hours_spent / Number_of_completed_HA)
+print("Курс:", Course_name + ",", "всего задач:" + str(Number_of_completed_HA) + ",",
+       "затрачено часов:" + str(Number_of_hours_spent) + ",", "среднее время выполнения",
+      Time_for_one_task, "часа.")
